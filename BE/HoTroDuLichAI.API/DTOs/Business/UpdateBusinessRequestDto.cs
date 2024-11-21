6028a0f349a337876d559d23c1161a840791a51b
NguyenCongTuan <nguyecongtuan@outlook.com>
2024-11-21 22:23:19 +0700

    update business logic be

--- BE/HoTroDuLichAI.API/DTOs/Business/UpdateBusinessRequestDto.cs
+++ BE/HoTroDuLichAI.API/DTOs/Business/UpdateBusinessRequestDto.cs
@@ -2,11 +2,11 @@ namespace HoTroDuLichAI.API
 {
     public class UpdateBusinessRequestDto
     {
-        public Guid Id { get; set; }
+        public Guid BusinessId { get; set; }
         public string BusinessName { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
-        public string Service { get; set; } = string.Empty;
-        public string BusinessContactPerson { get; set; } = string.Empty;
-        public BusinessContactPropertyDto BusinessContactProperty { get; set; } = null!;
+        public BusinessServiceProperty BusinessServiceProperty { get; set; } = new BusinessServiceProperty();
+        public BusinessContactProperty BusinessContactProperty { get; set; } = null!;
+        public string FileId { get; set; }  = string.Empty;
     }
 }
